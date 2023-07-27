ad144bdc89c6c2c82451cc5f9f65c7361788e81e
Aswinjithkukku <aswinjitht5@gmail.com>
2023-07-27 06:02:14 +0530

    update product

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -10,6 +10,19 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+type ProductStruct struct {
+	ShortName          string  `json:"shortName"`
+	LongName           string  `json:"longName"`
+	Cost               int     `json:"cost"`
+	Price              int     `json:"price"`
+	DiscountType       *string `json:"discountType"`
+	DiscountPrice      *int    `json:"discountPrice"`
+	Description        string  `json:"description"`
+	DealerName         string  `json:"dealerName"`
+	DealerPlace        string  `json:"dealerPlace"`
+	ProductDestination string  `json:"productDestination"`
+}
+
 func AdminCreateProduct(c *gin.Context) {
 
 	var product models.Products
@@ -24,7 +37,9 @@ func AdminCreateProduct(c *gin.Context) {
 
 	validate := validator.New()
 	if err := validate.Struct(product); err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
 		c.Abort()
 		return
 	}
@@ -44,6 +59,44 @@ func AdminCreateProduct(c *gin.Context) {
 		"success": true,
 		"product": product,
 	})
+
+}
+
+func AdminUpdateProduct(c *gin.Context) {
+	productId := c.Param("productid")
+
+	var body ProductStruct
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	var product models.Products
+
+	result := initializer.DB.First(&product).Where("id = ?", productId)
+
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": result.Error,
+		})
+		c.Abort()
+		return
+	}
+
+	product.ShortName = body.ShortName
+	product.LongName = body.LongName
+	product.Cost = body.Cost
+	product.Price = body.Price
+	product.DiscountType = body.DiscountType
+	product.DiscountPrice = body.DiscountPrice
+	product.Description = body.Description
+	product.DealerName = body.DealerName
+	product.DealerPlace = body.DealerPlace
+	product.ProductDestination = body.ProductDestination
+
+	initializer.DB.Save(&product)
 
 }
 
--- routes/admin.go
+++ routes/admin.go
@@ -9,6 +9,7 @@ func AdminRoute(router *gin.Engine) {
 	admin := router.Group("/api/admin")
 	{
 		admin.POST("/createproduct", controllers.AdminCreateProduct)
+		admin.POST("/updateproduct/:productid", controllers.AdminUpdateProduct)
 		admin.GET("/ping", controllers.Ping)
 	}
 }
